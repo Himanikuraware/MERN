4c957d9f9298bd1306ee36fa643f3bc04749cf87
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-09 19:49:57 +0530

    Created User routes

---
+++ backend/controllers/users-controllers.js
@@ -0,0 +1,49 @@
+const { v4: uuidv4 } = require("uuid");
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Himani",
+    email: "himani@gmail.com",
+    password: "himani",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("Couldn't create user, email already exists", 422);
+  }
+
+  const createduser = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+  };
+  DUMMY_USERS.push(createduser);
+  res.status(201).json({ user: createduser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError(
+      "Couldn't identify user, please signup if you don't have an account",
+      401
+    );
+  }
+  res.json({ message: "Logged in successfully!" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
