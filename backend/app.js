e3e0443fa238505cf509f4a15e5a6db7ad740bad
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-09 15:01:20 +0530

    Handled errors using middleware function

--- backend/app.js
+++ backend/app.js
@@ -6,4 +6,13 @@ const placesRoutes = require("./routes/places-routes");
 const app = express();
 app.use("/api/places", placesRoutes);
 
+//the function which has 4 parameters in express is recognized as a error handling middleware function.
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
 app.listen(5000);
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -20,12 +20,28 @@ const DUMMY_PLACES = [
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  if (!place) {
+    const error = new Error("Couldn't find a place for the provided id");
+    error.code = 404;
+    throw error;
+    // res
+    //   .status(404)
+    //   .json({ message: "Couldn't find a place for the provided id" });
+  }
   res.json({ place });
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find((p) => p.creator === userId);
+  if (!place) {
+    const error = new Error("Couldn't find a place for the provided user id");
+    error.code = 404;
+    return next(error);
+    // res
+    //   .status(404)
+    //   .json({ message: "Couldn't find a place for the provided user id" });
+  }
   res.json({ place });
 });
 
