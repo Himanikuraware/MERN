c961c778d79795298310fb11be232ef810c69c7f
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-09 20:37:42 +0530

    Completed the nodejs part

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 
 // Controllers have the middleware functions and the logic.
 
@@ -47,12 +48,18 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, Please check your data", 422);
+    return next(new HttpError("Invalid inputs passed, Please check your data", 422));
   }
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
   const createdPlace = {
     id: uuidv4(),
     title,
---
+++ backend/util/location.js
@@ -0,0 +1,10 @@
+const API_KEY = "test";
+
+const getCoordsForAddress = (address) => {
+  return {
+    lat: 27.1751448,
+    lng: 78.0395673,
+  };
+};
+
+module.exports = getCoordsForAddress;
