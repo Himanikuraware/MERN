1cbd7c663f3e52f8e0b1a0657e6be9232b3f2433
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-15 14:34:53 +0530

    Established the connection to the database

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -26,4 +27,9 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect('mongodb+srv://HimaniTest:himani@cluster0.2tfmflc.mongodb.net/places')
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
