63b88a5c290e626e7a828edb76352691382424bf
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-15 23:58:47 +0530

    Resolved CORS error

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  // Resolving CORS error.
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -28,7 +39,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://HimaniTest:himani@cluster0.2tfmflc.mongodb.net/mern')
+  .connect("mongodb+srv://HimaniTest:himani@cluster0.2tfmflc.mongodb.net/mern")
   .then(() => {
     app.listen(5000);
   })
