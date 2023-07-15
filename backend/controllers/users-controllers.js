f8b760f4524b11387ef51a129d2845955b0b5036
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-15 17:47:06 +0530

    Added User login

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -58,14 +58,18 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      "Couldn't identify user, please signup if you don't have an account",
-      401
-    );
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Logging in failed", 500);
+    return next(error);
+  }
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Invalid email or password", 401);
+    return next(error);
   }
   res.json({ message: "Logged in successfully!" });
 };
