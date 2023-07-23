d55bc7eeba44f9b57d838824548abd7c66731b2c
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-23 13:50:06 +0530

    Generated JWT tokens on BE.

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,8 +1,10 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
+const { create } = require("../models/user");
 
 const getUsers = async (req, res, next) => {
   //It will give all the information except password.
@@ -61,7 +63,24 @@ const signup = async (req, res, next) => {
     const error = new HttpError("Signing up failed, please try again", 500);
     return next(error);
   }
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: createdUser.id,
+        email: createdUser.email,
+      },
+      "secret_key",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again", 500);
+    return next(error);
+  }
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -94,9 +113,25 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: existingUser.id,
+        email: existingUser.email,
+      },
+      "secret_key",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Logging in failed, please try again", 500);
+    return next(error);
+  }
+
   res.json({
-    message: "Logged in successfully!",
-    user: existingUser.toObject({ getters: true }),
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
   });
 };
 
