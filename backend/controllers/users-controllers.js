2bdfab1bace0128ff9ba56fe36497763155e6470
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-23 13:37:55 +0530

    Created hashed password with bcrypt in BE.

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -39,11 +40,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Couldn't create a new user", 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: [],
   });
   try {
@@ -64,10 +73,27 @@ const login = async (req, res, next) => {
     const error = new HttpError("Logging in failed", 500);
     return next(error);
   }
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError("Invalid email or password", 401);
     return next(error);
   }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Couldn't logged you in, email or password is not correct!",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError("Invalid email or password", 401);
+    return next(error);
+  }
+
   res.json({
     message: "Logged in successfully!",
     user: existingUser.toObject({ getters: true }),
