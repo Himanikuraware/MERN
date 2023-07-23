694af37a46e9b47fb2eff4cd19c8934705c43919
Himanikuraware <kurawarehimani26@gmail.com>
2023-07-23 15:34:58 +0530

    Managed session expiration and auto-logout on FE.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -13,17 +13,62 @@ import { AuthContext } from "./shared/context/auth-context";
 import Auth from "./user/pages/Auth";
 import User from "./user/pages/User";
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState();
+  const [tokenExpirationTime, setTokenExpirationTime] = useState();
   const [userId, setUserId] = useState();
-  const login = useCallback((uid, token) => {
+
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+    // React by default sanitizes anything you output and render on the screen so we have cross site scripting attack protection in react so we can use localstorage to store our tokens.
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); // Getting the the expiration time of the token 1 hour which we set on BE.
+    setTokenExpirationTime(tokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
+
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationTime(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationTime) {
+      const remainingTime =
+        tokenExpirationTime.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationTime]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
+
   let routes;
   if (token) {
     // Switch:- It is user to switch between any routes because if we not use this then after first route execution it will redirect us to the home page again we we enter any other route
@@ -62,7 +107,9 @@ const App = () => {
     );
   }
   return (
-    <AuthContext.Provider value={{ isLoggedIn: !!token, token, userId, login, logout }}>
+    <AuthContext.Provider
+      value={{ isLoggedIn: !!token, token, userId, login, logout }}
+    >
       <Router>
         <MainNavigation />
         <main>{routes}</main>
